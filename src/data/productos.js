5bf872d6ada5b97447f8dba63bf65d1a45c41d69
emilyaumala <emilyaumala1@gmail.com>
2026-04-08 15:42:59 -0500

    Add new piano products to productos.js
    Added new piano products with details and images.

--- src/data/productos.js
+++ src/data/productos.js
@@ -9,6 +9,7 @@ export const productos = [
     imagenes: [
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/v1774370534/1_an5fbc.jpg",
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/v1774370533/2_yiw5zm.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1774370533/2_yiw5zm.jpg",
     ],
     whatsapp: "593999925027",
   },
@@ -18,7 +19,7 @@ export const productos = [
     tipo: "Piano Vertical",
     /*precio: "$4,200",*/
     descripcion:
-      "Piano Yamaha vertical negro",
+      "Piano Yamaha vertical. Color: negro, vino",
     imagenes: [
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/f_auto,q_auto/1_wzmvzr",
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/v1774368632/2_hatgee.jpg",
@@ -95,8 +96,84 @@ export const productos = [
     imagenes: [
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/v1774370925/1_ywahjq.jpg",
       "https://res.cloudinary.com/dgnzr1i4p/image/upload/v1774370927/2_mfpmzy.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679962/3_mq91ns.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679963/4_vbw2zo.jpg"
     ],
     whatsapp: "593999925027",
   },
-  
+  {
+    id: 8,
+    nombre: "Ballindamm B-123",
+    tipo: "Piano Vertical",
+    /*precio: "$18,500",*/
+    descripcion:
+      "Piano vertical Ballindamm B-123.",
+    imagenes: [
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679909/1_sfhrwb.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679909/2_qniq6t.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679911/3_tqq0l8.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679911/4_g2yxdr.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679912/5_q6ow4l.jpg",
+    ],
+    whatsapp: "593999925027",
+  },
+  {
+    id: 9,
+    nombre: "Atlas NA101",
+    tipo: "Piano Vertical",
+    /*precio: "$18,500",*/
+    descripcion:
+      "Piano vertical Atlas NA101 vino.",
+    imagenes: [
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679908/1_mxud4x.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679908/2_ioplyl.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679908/3_fnbgj0.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679909/4_qmhkyh.jpg",
+    ],
+    whatsapp: "593999925027",
+  },
+  {
+    id: 10,
+    nombre: "Carl Seiler 133D",
+    tipo: "Piano Vertical",
+    /*precio: "$18,500",*/
+    descripcion:
+      "Piano vertical Carl Seiler 133D vino.",
+    imagenes: [
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679915/1_f64dme.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679916/2_wfdunq.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679917/3_wpvtx1.jpg",
+    ],
+    whatsapp: "593999925027",
+  },
+    {
+    id: 11,
+    nombre: "Wagner W.2",
+    tipo: "Piano Vertical",
+    /*precio: "$18,500",*/
+    descripcion:
+      "Piano vertical Wagner W.2 vino.",
+    imagenes: [
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679912/1_ttiobe.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679913/2_y4u90s.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679914/3_svzvg8.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679915/4_hdmumv.jpg",
+    ],
+    whatsapp: "593999925027",
+  },
+      {
+    id: 12,
+    nombre: "Eterna N0.G3",
+    tipo: "Piano de Cola",
+    /*precio: "$18,500",*/
+    descripcion:
+      "Piano de cola Eterna N0.G3 negro.",
+    imagenes: [
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679929/2_djv0mg.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679928/1_zpuzer.jpg",
+      "https://res.cloudinary.com/dgnzr1i4p/image/upload/q_auto/f_auto/v1775679931/3_zhy80d.jpg",
+    
+    ],
+    whatsapp: "593999925027",
+  },
 ];
